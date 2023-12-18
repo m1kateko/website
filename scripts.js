d81282849614d213ed9b4f533c4148526aa6949c
m1kateko <142901582+m1kateko@users.noreply.github.com>
2023-12-18 18:39:03 +0200

    First upload
    Testing the site on mobile and stuff.

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const leftContainer = document.getElementById('left-image-container');
+const rightContainer = document.getElementById('right-image-container');
+
+leftContainer.addEventListener('mouseover', function() {
+  rightContainer.querySelector('.overlay-right').style.opacity = '0.8';
+});
+
+leftContainer.addEventListener('mouseout', function() {
+  rightContainer.querySelector('.overlay-right').style.opacity = '0';
+});
+
+rightContainer.addEventListener('mouseover', function() {
+  leftContainer.querySelector('.overlay-left').style.opacity = '0.8';
+});
+
+rightContainer.addEventListener('mouseout', function() {
+  leftContainer.querySelector('.overlay-left').style.opacity = '0';
+});
