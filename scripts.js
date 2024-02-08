21296e1edf3dd5c669027016edc17371c84fbb89
m1kateko <142901582+m1kateko@users.noreply.github.com>
2024-02-08 22:18:59 +0200

    Major font changes and minifying CSS

--- scripts.js
+++ scripts.js
@@ -22,18 +22,41 @@ document.addEventListener('DOMContentLoaded', function() {
   const menuIconClose = document.querySelector('.menu-close-icon');
   const menu = document.querySelector('.menu');
 
+  // Function to close the menu
+  function closeMenu() {
+    menu.classList.remove('active');
+    menuIconClose.style.display = 'none';
+    menuIconOpen.style.display = 'block';
+  }
+
+  // Event listener for opening the menu
   menuIconOpen.addEventListener('click', function() {
     menu.classList.toggle('active');
     menuIconOpen.style.display = 'none';
     menuIconClose.style.display = 'block';
   });
 
-  menuIconClose.addEventListener('click', function() {
-    menu.classList.toggle('active');
-    menuIconClose.style.display = 'none';
-    menuIconOpen.style.display = 'block';
+  // Event listener for closing the menu when clicking outside
+  document.addEventListener('click', function(event) {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnMenuIcon = menuIconOpen.contains(event.target);
+    
+    if (!isClickInsideMenu && !isClickOnMenuIcon) {
+      closeMenu();
+    }
+  });
+
+  // Event listener for closing the menu when scrolling
+  document.addEventListener('scroll', function() {
+    closeMenu();
+  });
+
+  // Event listener for closing the menu when resizing the window
+  window.addEventListener('resize', function() {
+    closeMenu();
   });
 });
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetch('quoteDB.json') // Fetch the JSON file
